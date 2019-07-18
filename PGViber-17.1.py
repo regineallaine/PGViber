aafdb9b743a31ef6e3d38376838ded45455f7b97
Reyes <rreyes53@dxc.com>
2019-07-19 01:41:33 +0800

    yahooooo

--- PGViber-17.1.py
+++ PGViber-17.1.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 import pymysql
 
 
+
 app = Flask(__name__)
 
 
@@ -48,63 +49,31 @@ def edit(empid):
         gender = request.form['gender']
         lastname = request.form['lastname']
         firstname = request.form['firstname']
+        # employeenumber = request.form['employeenumber']
+        emailaddress = request.form['emailaddress']
+        password = request.form['password']
         # sql="UPDATE employees SET last_name = '" + lastname +"' WHERE employee_number = %s;"
         conn = pymysql.connect(host="localhost", user="root", passwd="1234", database="db_info")
-        # password = request.form['password']
-        # first_name = request.form['firstname']
-        # last_name = request.form['lastname']
-        # gender = request.form['gender']
-        # mobile_number = request.form['mobile_number']
-
-        
-
         B_cur=conn.cursor()
-        # B_cur.execute(sql)
-        # cur = pymysql.connection.cursor()
-        B_cur.execute("UPDATE employees SET last_name=%s, first_name=%s, gender=%s, mobile_number=%s WHERE employee_number=%s", (lastname,firstname,gender,phonenumber,empnum))
-        # pymysql.connection.commit()
-        #  email password  first_name gender birthdate mobile_number
-        # # try:
+        B_cur.execute("UPDATE employees SET email_address=%s, password=%s, last_name=%s, first_name=%s, gender=%s, mobile_number=%s WHERE employee_number=%s", (emailaddress,password,lastname,firstname,gender,phonenumber,empnum))
         
-        B_cur.close()
-        conn.commit()
-        return "Success"
-    # def getvaluenew():    
-#     empnum = [(request.form['empnum'])]
-#     conn = pymysql.connect(host="localhost", user="root", passwd="1234", database="db_info")
-#     B_cur=conn.cursor()
-#     sql="UPDATE employees SET password=%s last_name='%s', first_name=%s, birthdate='%s' gender='%s', mobile_number=%s WHERE employee_number = %s ",
-#     (password.get(), lastname.get(), firstname.get(), gender.get(), birthday.get(), phonenumber.get())
-#     B_cur.execute(sql)
+        # B_cur.close()
         
-#     conn.commit()    
-#     return render_template('PGViber-17.2.html', a1=employeenumber, a2=emailaddress, a3=pas, a4=lname, a5=fname, a6=gend, a7=phone, a8=bday)
-# def update_employees():
-#     # read database configuration
-#     db_info = read_db_info()
+        # return "Success"
 
-    # sql = """ UPDATE employees
-    #             SET email = %s, password = %s, last_name = %s, first_name = %s, birthdate = %s, gender = %s, mobile_number = %s
-    #             WHERE employee_number = %s """
- 
-#     data = (email, password, last_name, first_name, birthdate, gender, mobile_number)
- 
-#     try:
-#         conn = MySQLConnection(**db_info)
- 
-#         # update book title
-#         cursor = conn.cursor()
-#         cursor.execute(sql, data)
- 
-#         # accept the changes
-#         conn.commit()
- 
-#     except Error as error:
-#         print(error)
- 
-#     finally:
-#         cursor.close()
-#         conn.close()
+        # print(row)
+        # employeenumber = row[0]
+        # emailaddress = row[1]
+        # pas = row[2]
+        # lname = row[3]
+        # fname = row[4]
+        # gend = row[5]
+        # phone = row[6]
+        # bday = row[7]
+        conn.commit()
+        return redirect(url_for('edit',empid=empnum))
+        # return render_template('PGViber-17.2.html', a1=employeenumber, a2=emailaddress, a3=pas, a4=lname, a5=fname, a6=gend, a7=phone, a8=bday)
+
 
 if __name__ =='__main__':
     app.run(debug=True)
